a1f9d24611ffd348888fa03a9a56c374c31ee9cb
Messias Junior <messiaslima.03@gmail.com>
2022-03-15 09:19:50 +0000

    chapter 3

--- geo-quiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ geo-quiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.bignerdranch.android.geoquiz
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageButton
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -63,6 +64,36 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
     private fun nextQuestion() {
         currentIndex = (currentIndex + 1) % questionBank.size
         updateQuestion()
@@ -74,22 +105,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
+        trueButton.isEnabled = !currentQuestion.wasAnswered
+        falseButton.isEnabled = !currentQuestion.wasAnswered
+        nextButton.isEnabled = currentIndex < (questionBank.size - 1)
+        prevButton.isEnabled = currentIndex >= 1
         questionTextView.setText(currentQuestion.textResId)
     }
 
     private fun submitAnswer(answer: Boolean) {
+        currentQuestion.wasAnswered = true
         val message = if (answer == currentQuestion.answer) {
+            currentQuestion.answeredCorrectly = true
             R.string.toast_correct
         } else {
             R.string.toast_incorrect
         }
 
         showToast(message)
+        updateQuestion()
+        checkShowResultEligibility()
+    }
+
+    private fun checkShowResultEligibility() {
+        if (questionBank.all { it.wasAnswered }) {
+            showResult()
+        }
+    }
+
+    private fun showResult() {
+        val ratio = questionBank.count { it.answeredCorrectly } / questionBank.size.toFloat()
+        val percentage = ratio * 100
+        showToast(getString(R.string.result, percentage))
     }
 
     private fun showToast(@StringRes message: Int) {
         val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
         toast.setGravity(Gravity.TOP, 0, 200)
         toast.show()
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 200)
+        toast.show()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
